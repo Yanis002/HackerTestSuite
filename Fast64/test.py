dedde4f429fd06b3c31895e31911edffe30b3356
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-10-29 19:17:00 +0100

    progress on decomp export-autobuild

--- Fast64/test.py
+++ Fast64/test.py
@@ -5,8 +5,12 @@ import addon_utils
 import argparse
 import os
 import sys
+import shutil
 
 from pathlib import Path
+
+
+INDENT = " " * 4
 
 
 class Utils:
@@ -44,26 +48,186 @@ class Utils:
         bpy.ops.outliner.orphans_purge(do_recursive=True)
 
 
+class DecompExporter:
+    def __init__(self, scene_name: str, is_single_file: bool, cs_total: int, room_total: int, entr_objs: list[bpy.types.Object], draw_config: str, title_card: str):
+        self.scene_name = scene_name
+        self.is_single_file = is_single_file
+        self.cs_total = cs_total
+        self.room_total = room_total
+        self.entr_objs = entr_objs
+        self.draw_config = draw_config
+        self.title_card = title_card
+
+        self.entr_base = f"ENTR_{self.scene_name.upper()}"
+        self.scene_id = f"SCENE_{self.scene_name.upper()}"
+
+    def get_entry_base(self, name: str, compress: bool = True):
+        segment = [f'name "{name}"']
+
+        if compress:
+            segment.append("compress")
+
+        segment.append("romalign 0x1000")
+        return segment
+    
+    def get_scene_entry(self):
+        name = f"{self.scene_name}_scene"
+        segment = self.get_entry_base(name)
+
+        if self.is_single_file:
+            includes = [f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}.o"']
+        else:
+            includes = [
+                f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_main.o"',
+                f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_col.o"',
+            ]
+
+            if self.cs_total > 0:
+                for i in range(self.cs_total):
+                    includes.append(f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_cs_{i}.o"')
+
+        segment.extend(includes)
+        segment.append("number 2")
+        return segment
+
+    def get_room_entry(self, room_index: int):
+        name = f"{self.scene_name}_room_{room_index}"
+        segment = self.get_entry_base(name)
+
+        if self.is_single_file:
+            includes = [f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}.o"']
+        else:
+            includes = [
+                f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_main.o"',
+                f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_model_info.o"',
+                f'include "$(BUILD_DIR)/assets/testsuite/scenes/{self.scene_name}/{name}_model.o"',
+            ]
+
+        segment.extend(includes)
+        segment.append("number 3")
+        return segment
+
+    def get_scene_entries(self):
+        scene_segment = f"beginseg\n{INDENT}" + f"\n{INDENT}".join(elem for elem in self.get_scene_entry()) + "\nendseg\n\n"
+
+        room_segments = str()
+        for i in range(self.room_total):
+            room_segment = f"beginseg\n{INDENT}" + f"\n{INDENT}".join(elem for elem in self.get_room_entry(i)) + "\nendseg\n"
+            room_segments += room_segment + "\n"
+
+        return scene_segment + room_segments
+
+    def get_map_select_entries(self):
+        select_entry = f"// ----- Scene '{self.scene_name}' -----\n"
+        
+        for i in range(len(self.entr_objs)):
+            select_entry += "{ " + f'"{self.scene_name} ({i})", (void*)MapSelect_LoadGame, {self.entr_base}_{i}' + " },\n"    
+
+        return select_entry + "\n"
+
+    def get_entrance_entries(self):
+        entrance_entry = f"// ----- Scene '{self.scene_name}' -----\n\n"
+
+        for i, entr_obj in enumerate(self.entr_objs):
+            assert i == entr_obj.ootEntranceProperty.spawnIndex, f"index mismatch for entrance '{entr_obj.name}', are the spawn indices correct?"
+
+            entrance_entry += (
+                f"// Spawn No. {i}, Empty Name: '{entr_obj.name}'\n" 
+                + f"DEFINE_ENTRANCE({self.entr_base}_{i}, {self.scene_id}, {i}, false, true, TRANS_TYPE_FADE_WHITE, TRANS_TYPE_FADE_WHITE)\n"
+            )
+
+            for j in range(1, 4):
+                entrance_entry += f"DEFINE_ENTRANCE({self.entr_base}_{i}_{j}, {self.scene_id}, {i}, false, true, TRANS_TYPE_FADE_WHITE, TRANS_TYPE_FADE_WHITE)\n"
+            
+            entrance_entry += "\n"
+
+        return entrance_entry
+
+    def get_scene_table_entry(self):
+        return f"DEFINE_SCENE({self.scene_name}_scene, {self.title_card}, {self.scene_id}, {self.draw_config}, 0, 0)\n"
+
+
 class Tests:
     """Hosts test functions to try fast64 features"""
 
-    def __init__(self, resources_path: Path, tests_path: Path):
+    def __init__(self, resources_path: Path, tests_path: Path, export_path: Path, out_path: Path):
         self.resources_path = resources_path
         self.tests_path = tests_path
+        self.export_path = export_path
+        self.out_path = out_path
+
+        print(f"Using:\n\tresources_path: {self.resources_path}\n\ttests_path: {self.tests_path}\n\texport_path: {self.export_path}\n\tout_path: {self.out_path}")
 
     def export(self, is_hackeroot: bool):
         """Finds each blend from `Fast64/tests/export/' opens them and tries to export the scene with and without single file enabled"""
 
-        export_path = self.tests_path / "export"
         decomp_type = "HackerOoT" if is_hackeroot else "oot"
+        out_decomp = self.out_path / decomp_type
+        inc_folder = out_decomp / "include" / "testsuite"
+        spec_folder = out_decomp / "spec"
+        spec_entries = str()
+        map_select_entries = str()
+        entrance_entries = str()
+        scene_entries = str()
 
-        # print(bpy.context.mode)
+        if out_decomp.exists():
+            shutil.rmtree(out_decomp)
+        out_decomp.mkdir(parents=True)
+        inc_folder.mkdir(parents=True)
+        spec_folder.mkdir(parents=True)
 
-        for blend in export_path.rglob("*.blend"):
+        for blend in (self.tests_path / "export").rglob("*.blend"):
+            name_single = f"{blend.stem}_{decomp_type.lower()}_singlefile"
+            name_multi = f"{blend.stem}_{decomp_type.lower()}_multifile"
+
             bpy.ops.wm.open_mainfile(filepath=str(blend))
             bpy.context.scene.ootDecompPath = str(self.resources_path / decomp_type)
-            Utils.export_scene(True, True, str(export_path / "out"), f"{blend.stem}_{decomp_type.lower()}_singlefile")
-            Utils.export_scene(False, True, str(export_path / "out"), f"{blend.stem}_{decomp_type.lower()}_multifile")
+
+            scene_objs = [obj for obj in bpy.data.objects if obj.type == "EMPTY" and obj.ootEmptyType == "Scene"]
+
+            for scene_obj in scene_objs:
+                scene_header = scene_obj.ootSceneHeader
+
+                room_objs = [obj for obj in scene_obj.children_recursive if obj.type == "EMPTY" and obj.ootEmptyType == "Room"]
+                entrance_objs = [obj for obj in scene_obj.children_recursive if obj.type == "EMPTY" and obj.ootEmptyType == "Entrance"]
+                entrance_objs.sort(key=lambda obj: obj.ootEntranceProperty.spawnIndex)
+
+                # TODO: fix issue with duplicated cutscene names
+                scene_header.writeCutscene = False
+                cs_objs = [
+                    # obj for obj in bpy.data.objects
+                    # if obj.type == "EMPTY" 
+                    # and obj.ootEmptyType == "Cutscene" 
+                    # and (scene_header.writeCutscene and scene_header.csWriteObject == obj or obj in scene_header.extraCutscenes)
+                ]
+
+                bpy.context.scene.ootSceneExportObj = scene_obj
+
+                Utils.export_scene(True, True, str((out_decomp / "assets" / "testsuite" / "scenes")), name_single)
+                exporter = DecompExporter(name_single, True, len(cs_objs), len(room_objs), entrance_objs, scene_header.sceneTableEntry.drawConfig, scene_header.title_card_name)
+                spec_entries += exporter.get_scene_entries()
+                map_select_entries += exporter.get_map_select_entries()
+                entrance_entries += exporter.get_entrance_entries()
+                scene_entries += exporter.get_scene_table_entry()
+
+                Utils.export_scene(False, True, str((out_decomp / "assets" / "testsuite" / "scenes")), name_multi)
+                exporter = DecompExporter(name_multi, False, len(cs_objs), len(room_objs), entrance_objs, scene_header.sceneTableEntry.drawConfig, scene_header.title_card_name)
+                spec_entries += exporter.get_scene_entries()
+                map_select_entries += exporter.get_map_select_entries()
+                entrance_entries += exporter.get_entrance_entries()
+                scene_entries += exporter.get_scene_table_entry()
+
+        path = spec_folder / "testsuite.inc"
+        path.write_text(spec_entries)
+
+        path = inc_folder / "map_select.h"
+        path.write_text(map_select_entries)
+
+        path = inc_folder / "entrance_table.h"
+        path.write_text(entrance_entries)
+
+        path = inc_folder / "scene_table.h"
+        path.write_text(scene_entries)
 
 
 def main(args):
@@ -73,9 +237,22 @@ def main(args):
     tests_path = Path(args.tests_path).resolve()
     assert tests_path.exists()
 
-    tests = Tests(resources_path, tests_path)
-    tests.export(False)
-    tests.export(True)
+    if args.export_path is not None:
+        export_path = Path(args.export_path).resolve()
+        assert export_path.exists()
+    else:
+        export_path = tests_path / "export"
+
+    tests = Tests(resources_path, tests_path, export_path, Path("./out").resolve())
+
+    print(f"Using mode: '{args.mode}'")
+
+    match args.mode:
+        case "export":
+            tests.export(False)
+            tests.export(True)
+        case _:
+            print(f"Unknown mode '{args.mode}'")
 
 
 if __name__ == "__main__":
@@ -90,6 +267,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Testing Playground")
     parser.add_argument(
+        "--mode",
+        dest="mode",
+        help="Operating mode",
+        required=True,
+    )
+    parser.add_argument(
         "--resources-path",
         dest="resources_path",
         help="Path to resources folder",
@@ -100,6 +283,12 @@ if __name__ == "__main__":
         dest="tests_path",
         help="Path to tests folder",
         required=True,
+    )
+    parser.add_argument(
+        "--export-path",
+        dest="export_path",
+        help="Export path to use",
+        required=False,
     )
 
     args = parser.parse_args(argv)
